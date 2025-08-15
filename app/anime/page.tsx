70a79ff3d2fc7e414caf64a3d7a094d946a76eb6
Tanaphat Partoom <76895893+ShadowsDuck@users.noreply.github.com>
2025-08-15 17:23:20 +0700

    - change searchParams Props

--- app/anime/page.tsx
+++ app/anime/page.tsx
@@ -2,23 +2,21 @@ import Search from "@/components/search";
 import CardSectionLoading from "@/components/loadings/card-section-loading";
 import { Suspense } from "react";
 import CardContent from "@/components/card-content";
-interface PageProps {
-  searchParams: Promise<{
-    page?: string;
-    limit?: string;
-  }>;
-}
 
-export default async function Page({ searchParams }: PageProps) {
-  const params = await searchParams;
-  const currentPage = Number(params.page) || 1;
-  const limit = Number(params.limit) || 24;
+export default async function Page({
+  searchParams,
+}: {
+  searchParams?: { [key: string]: string | string[] | undefined };
+}) {
+  // const params = await searchParams;
+  const currentPage = Number(searchParams?.page) || 1;
+  const limit = Number(searchParams?.limit) || 24;
 
   return (
     <div className="page-wrapper-layout">
       <Search />
 
-      <Suspense key={currentPage} fallback={<CardSectionLoading length={24} />}>
+      <Suspense fallback={<CardSectionLoading length={24} />}>
         <CardContent currentPage={currentPage} limit={limit} type="anime" />
       </Suspense>
     </div>
